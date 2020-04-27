0426456bf9d1717c8716f85088e32b315019cec1
Maxim Tamarlakov <maximtamgoo@gmail.com>
2020-04-27 17:52:56 -0400

    renamed export of header component

--- react-client/src/components/Header/Header.jsx
+++ react-client/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../style/Header.css'
 
-const App = () => {
+const Header = () => {
   return (
     <div className="header">
       Header
@@ -9,4 +9,4 @@ const App = () => {
   )
 }
 
-export default App
+export default Header
