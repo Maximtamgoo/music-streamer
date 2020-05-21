ac42581ee27212b413edf96fb702ff63f8550112
Maxim Tamarlakov <maximtamgoo@gmail.com>
2020-05-20 20:45:38 -0400

    rearranged components via css grid

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import './App.css'
-import Header from './Header/Header'
-import SideBar from './SideBar/SideBar'
 import SongsPanel from './SongsPanel/SongsPanel'
 import Player from './Player/Player'
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <div className="middle">
-        <SideBar />
-        <SongsPanel />
+      <div class="side-bar">
+        side-bar
       </div>
+      <div class="header">
+        header
+        {/* <UploadsDropdown /> */}
+        {/* <AccountDropdown /> */}
+      </div>
+      <SongsPanel />
       <Player />
     </div>
   )
